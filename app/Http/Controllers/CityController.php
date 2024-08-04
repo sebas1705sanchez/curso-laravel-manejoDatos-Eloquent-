55150685e1c0bdabbaebcf8051a6bf5bf8b0619a
sebas1705sanchez <s.sanchez8@utp.edu.co>
2024-08-04 17:49:01 -0500

    se crearon componenetes para extraer los datos de la base de datos de Cities, users, companies

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\City;
+
+class CityController extends Controller
+{
+    public function index() {
+        $city = City::all();
+        return response()->json($city);
+    }
+}
